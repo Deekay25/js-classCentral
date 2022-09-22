ae7c080892e39959716b1a7e15d6431cc4168ea1
adnan ahmad <ahmadadnan2235@gmail.com>
2022-09-23 00:18:37 +0100

    const keywrod

---
+++ basicJs/const.js
@@ -0,0 +1,10 @@
+//just like let, const can only be declared once
+// with const you can't re assign to that var just like let will allow
+// use const when you don't want variable to change
+//const is usually capital letters e.g FCC, OLDCATS
+
+const animal = "cat"
+animal = "dog";
+console.log(dog);
+
+//above code will throw an error
