01aa35d5403e7966b578dedef5232746822704c3
adnan ahmad <ahmadadnan2235@gmail.com>
2022-09-24 00:53:37 +0100

    bracket notation to find strings

---
+++ basicJs/bracketNotation.js
@@ -0,0 +1,7 @@
+//bracket notation is used to find an index or position of a string
+let lastNameLength = 0;
+const lastName = "lovelace";
+
+lastNameLength = lastName[0];
+
+console.log(lastNameLength);
