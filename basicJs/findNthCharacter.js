2ed66765f2c6ce6c603672b509c4bf827b721d8b
adnan ahmad <ahmadadnan2235@gmail.com>
2022-09-24 01:08:41 +0100

    find last character in a string

--- basicJs/findNthCharacter.js
+++ basicJs/findNthCharacter.js
@@ -1,4 +1,4 @@
-//using .length to find a length of a string in js
+
 
 let lastNameLength = 0;
 const lastName = "lovelace";
---
+++ basicJs/lastCharacterStr.js
@@ -0,0 +1,10 @@
+//using .length to find a length of a string in js
+// .length - 1 to find the last character
+
+let lastNameLength = 0;
+const lastName = "lovelace";
+
+//lastNameLength = lastName.length-1;//find the position of the last character
+lastNameLength = lastName[lastName.length - 1]; //prints the last character in a string
+
+console.log(lastNameLength);
