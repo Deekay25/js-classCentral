592804a9553c5d7a9584fe2886c596bf6b24faab
adnan ahmad <ahmadadnan2235@gmail.com>
2022-09-27 13:26:10 +0100

    counting exercise done

--- basicJs/functions/countingExercise.js
+++ basicJs/functions/countingExercise.js
@@ -19,13 +19,39 @@ let count = 0;
 
 function cc(card) {
   // Only change code below this line
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count--;
+      break;
+  }
 
-  return "Change Me";
+  let holdbet = "Hold";
+  if (count > 0) {
+    holdbet = "Bet";
+  }
+  return count + " " + holdbet;
   // Only change code above this line
 }
 
-cc(2);
-cc(3);
-cc(7);
-cc("K");
-cc("A");
+// cc(2);
+// cc(3);
+// cc(7);
+// cc("K");
+// cc("A");
+
+// console.log(cc(10));
+// console.log(cc("J"));
+// console.log(cc("Q"));
+// console.log(cc("K"));
+console.log(cc("A"));
