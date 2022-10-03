1b49048abd49a62d7e7303a524cc48e34d5a2a55
adnan ahmad <ahmadadnan2235@gmail.com>
2022-10-03 20:22:49 +0100

    while loop intro

--- basicJs/loops/while.js
+++ basicJs/loops/while.js
@@ -1,7 +1,11 @@
-const arr = [];
+const myarray = [];
 
-let i = 0;
-while (i < 5) {
-   console.log(arr.push(i));
-   i++;
+let i = 5;
+
+while (i >= 0){
+    console.log(myarray.push(i)) // this will print how many times the loops executed in this case 6 times
+    console.log(myarray); // this will print how the array will be incrementing
+    i--;
 }
+
+console.log(myarray); // this will print the final array
