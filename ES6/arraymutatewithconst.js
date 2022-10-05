99f9d39bbe141ea8343486d3870c9c30b8a854d3
adnan ahmad <ahmadadnan2235@gmail.com>
2022-10-06 00:30:09 +0100

    mutating an array

---
+++ ES6/arraymutatewithconst.js
@@ -0,0 +1,16 @@
+//important
+
+// An array is declared as const s = [5, 7, 2]. 
+// Change the array to [2, 5, 7] using various element assignments.
+
+const s = [5, 7, 2];
+function editInPlace() {
+  // Only change code below this line
+  let newval = s.pop();
+  s.unshift(newval);
+  return s;
+  // Using s = [2, 5, 7] would be invalid
+
+  // Only change code above this line
+}
+console.log(editInPlace());
