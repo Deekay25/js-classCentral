ec87224faf2d15df5adc748c5c90f0d6398d635c
adnan ahmad <ahmadadnan2235@gmail.com>
2022-10-12 23:24:46 +0100

    match same string literal

--- regex/matchLiteralString.js
+++ regex/matchLiteralString.js
@@ -8,3 +8,7 @@
 let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
 let waldoRegex = /Waldo/; // Change this line
 let result = waldoRegex.test(waldoIsHiding);
+let wrong = /aldo/;
+console.log(result)
+console.log(wrong.test(waldoIsHiding)) // returns true
+// it returns true because "aldo" is a match literal in the string WaldoIsHiding
