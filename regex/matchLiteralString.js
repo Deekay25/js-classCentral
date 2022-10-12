a483de4b46c409ea39675d4073b514bb27a2e54a
adnan ahmad <ahmadadnan2235@gmail.com>
2022-10-12 23:19:58 +0100

    match string literal

---
+++ regex/matchLiteralString.js
@@ -0,0 +1,10 @@
+// Complete the regex waldoRegex to find "Waldo" in the string waldoIsHiding with a literal match.
+
+// problem
+// let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+// let waldoRegex = /search/; // Change this line
+// let result = waldoRegex.test(waldoIsHiding);
+// solution
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /Waldo/; // Change this line
+let result = waldoRegex.test(waldoIsHiding);
--- regex/usingTestMethod.js
+++ regex/usingTestMethod.js
@@ -2,10 +2,12 @@
 
 // the test method test wether a string exists using regular expresion
 // returns true if it exists and return false if it does not
+// regular expressions are case sensitive
 
 let myString = "Hello, World!";
 let myRegex = /Hello/; // /Hello/ means it is a regular expression
 let rex = /mine/;
 let result = myRegex; // Change this line
+//result.test(myString) means that check if the string "Hello" exists in myString
 console.log(result.test(myString)); // returns true since the string hello exists in the string
 console.log(rex.test(myString)); // returns false
