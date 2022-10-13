e62ba284556684ad7f36c85748864bce5740fcbb
adnan ahmad <ahmadadnan2235@gmail.com>
2022-10-13 14:13:25 +0100

    group same characters and return

---
+++ regex/groupMatchcharacters.js
@@ -0,0 +1,7 @@
+// For example, /a+/g would find one match in abc and return ["a"].
+//  Because of the +, it would also find a single match in aabc and return ["aa"].
+
+let difficultSpelling = "Mississippi";
+let myRegex = /s+/gi; // Change this line
+let result = difficultSpelling.match(myRegex);
+console.log(result);
