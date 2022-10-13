35a0859e32608d3e5622d47bba88af94767e2b85
adnan ahmad <ahmadadnan2235@gmail.com>
2022-10-13 14:11:22 +0100

    non matching characters

---
+++ regex/nonMatchRegex.js
@@ -0,0 +1,11 @@
+//negated character sets. denonted by ^
+
+// the program below all that in the string
+//except 0-9 and vowels
+let quoteSample = "3 blind mice.";
+let myRegex = /[^0-9aioeu]/gi; // Change this line
+let result = quoteSample.match(myRegex); // Change this line
+let result2 = myRegex.test(quoteSample);
+console.log(result);
+console.log(result2);
+
